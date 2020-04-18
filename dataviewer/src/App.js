a9278c8d6f533ef2e3bcf53e91d00276af15af9c
Genzo Namikawa <genzo.namikawa@jacks.sdstate.edu>
2020-04-18 10:36:43 -0500

    Part 2

--- dataviewer/src/App.js
+++ dataviewer/src/App.js
@@ -3,7 +3,6 @@ import LandingPage from "./pages/LandingPage";
 import {Grid, Container, Image} from "semantic-ui-react";
 import LoginForm from "./components/LoginForm"
 import LoginPage from "./pages/LoginPage"
-import ThemeBg from "./components/ThemeBg"
 import ErrorPage from "./pages/ErrorPage"
 
 let ActivePage = "landing"
